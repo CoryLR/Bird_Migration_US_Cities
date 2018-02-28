3c2628e5a7ed357ba4e7463885e1a60a784e40af
CoryOp <crahman@wisc.edu>
2018-02-27 22:53:12 -0500

    Boilerplate Leaflet + Basemap
    Created HTML, CSS and JS files; set up boilerplate Leaflet and basemap.

---
+++ js/leaflet.js
@@ -0,0 +1,8 @@
+// Initialize Leaflet map
+var mymap = L.map('map').setView([38, -96], 5);
+
+// Basemap
+var Esri_WorldGrayCanvas = L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/Canvas/World_Light_Gray_Base/MapServer/tile/{z}/{y}/{x}', {
+    attribution: 'Tiles &copy; Esri &mdash; Esri, DeLorme, NAVTEQ',
+    maxZoom: 16
+}).addTo(mymap);
